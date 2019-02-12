da97882449e652200fd9394fc511a7c441216cf2
Nazarii-Ivan Stosyk <fantombeowulf@gmail.com>
2019-02-12 23:23:27 +0200

    Delete isSmaller.js

--- FLX_homework_8/homework/js/isSmaller.js
+++
@@ -1,7 +0,0 @@
-function isBigger(FirstStatement,SecondStatement){
-    return FirstStatement>SecondStatement;
-}
-function isSmaller(FirstStatement,SecondStatement){
-    return isBigger(SecondStatement,FirstStatement);
-}
-isSmaller(5,-1);
