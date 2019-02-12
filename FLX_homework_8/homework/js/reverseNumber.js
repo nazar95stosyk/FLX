69fc7638595a3db50b6130fe25e23658e91daa48
Nazarii-Ivan Stosyk <fantombeowulf@gmail.com>
2019-02-12 23:23:38 +0200

    Delete reverseNumber.js

--- FLX_homework_8/homework/js/reverseNumber.js
+++
@@ -1,7 +0,0 @@
-function reverse_a_number(n){
-	n = n + "";
-	return parseInt(n.split("").reverse().join("")) * Math.sign(n);
-}
-reverse_a_number(-456);
-reverse_a_number(100000);
-reverse_a_number(123);
